4059f312c71d1722e6fd531d98360d259d867f6c
bbewx <wjl_csu@163.com>
2018-06-24 11:51:03 +0800

    Spring框架Demo

---
+++ Spring/src/aop/Bicycle.java
@@ -0,0 +1,20 @@
+package aop;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Administrator on 2018\6\22 0022.
+ */
+@Component
+public class Bicycle {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
