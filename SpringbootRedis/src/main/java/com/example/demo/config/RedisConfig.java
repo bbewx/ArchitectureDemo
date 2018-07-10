df66ad240eabd516ea992448db6f95e88d8470e8
bbewx <wjl_csu@163.com>
2018-07-10 23:21:35 +0800

    第一次提交RedisDemo

---
+++ SpringbootRedis/src/main/java/com/example/demo/config/RedisConfig.java
@@ -0,0 +1,186 @@
+package com.example.demo.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+import java.lang.reflect.Method;
+import java.time.Duration;
+
+/**
+ * Created by Administrator on 2018\5\5 0005.
+ */
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Value("${spring.redis.database}")
+    private int database;
+
+    @Value("${spring.redis.lettuce.pool.max-idle}")
+    private int maxIdle;
+
+    @Value("${spring.redis.lettuce.pool.min-idle}")
+    private int minIdle;
+
+    /**
+     *  注解@Cache的管理器，设置过期时间的单位是秒
+     * @Description:
+     * @return
+     */
+    @Bean
+    CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+//        //user信息缓存配置
+//        RedisCacheConfiguration userCacheConfiguration
+//                = RedisCacheConfiguration.defaultCacheConfig().entryTtl(
+//                        Duration.ofMinutes(30)).disableCachingNullValues().prefixKeysWith("user");
+//        //product信息缓存配置
+//        RedisCacheConfiguration productCacheConfiguration
+//                = RedisCacheConfiguration.defaultCacheConfig().entryTtl(
+//                        Duration.ofMinutes(10)).disableCachingNullValues().prefixKeysWith("product");
+//        Map<String, RedisCacheConfiguration> redisCacheConfigurationMap = new HashMap<>();
+//        redisCacheConfigurationMap.put("user", userCacheConfiguration);
+//        redisCacheConfigurationMap.put("product", productCacheConfiguration);
+        //初始化一个RedisCacheWriter
+        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(connectionFactory);
+
+
+        //设置CacheManager的值序列化方式为JdkSerializationRedisSerializer,但其实RedisCacheConfiguration默认就是使用StringRedisSerializer序列化key，JdkSerializationRedisSerializer序列化value,所以以下注释代码为默认实现
+        //ClassLoader loader = this.getClass().getClassLoader();
+        //JdkSerializationRedisSerializer jdkSerializer = new JdkSerializationRedisSerializer(loader);
+        //RedisSerializationContext.SerializationPair<Object> pair = RedisSerializationContext.SerializationPair.fromSerializer(jdkSerializer);
+        //RedisCacheConfiguration defaultCacheConfig=RedisCacheConfiguration.defaultCacheConfig().serializeValuesWith(pair);
+
+
+        RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig();
+        //设置默认超过期时间是30秒
+        defaultCacheConfig.entryTtl(Duration.ofSeconds(30));
+        //初始化RedisCacheManager
+//        RedisCacheManager cacheManager = new RedisCacheManager(
+//                redisCacheWriter, defaultCacheConfig, redisCacheConfigurationMap);
+        RedisCacheManager cacheManager = new RedisCacheManager(
+                redisCacheWriter, defaultCacheConfig);
+        return cacheManager;
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        StringRedisTemplate template = new StringRedisTemplate(factory);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer
+                = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    /**
+     * redis模板，存储关键字是字符串，值是Jdk序列化
+     * @Description:
+     * @param factory
+     * @return
+     */
+//    @Bean
+//    public RedisTemplate<?,?> redisTemplate(RedisConnectionFactory factory) {
+//        RedisTemplate<?,?> redisTemplate = new RedisTemplate<>();
+//        redisTemplate.setConnectionFactory(factory);
+//        //key序列化方式;但是如果方法上有Long等非String类型的话，会报类型转换错误；
+//        RedisSerializer<String> redisSerializer = new StringRedisSerializer();//Long类型不可以会出现异常信息;
+//        redisTemplate.setKeySerializer(redisSerializer);
+//        redisTemplate.setHashKeySerializer(redisSerializer);
+//
+//        //JdkSerializationRedisSerializer序列化方式;
+//        JdkSerializationRedisSerializer jdkRedisSerializer=new JdkSerializationRedisSerializer();
+//        redisTemplate.setValueSerializer(jdkRedisSerializer);
+//        redisTemplate.setHashValueSerializer(jdkRedisSerializer);
+//        redisTemplate.afterPropertiesSet();
+//        return redisTemplate;
+//    }
+
+    /**
+     *  注解@Cache key生成规则
+     */
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
+
+    /**
+     * redis数据操作异常处理
+     * 这里的处理：在日志中打印出错误信息，但是放行
+     * 保证redis服务器出现连接等问题的时候不影响程序的正常运行，使得能够出问题时不用缓存
+     * @return
+     */
+    @Bean
+    @Override
+    public CacheErrorHandler errorHandler() {
+        CacheErrorHandler cacheErrorHandler = new CacheErrorHandler() {
+            @Override
+            public void handleCacheGetError(RuntimeException e, Cache cache, Object key) {
+                logger.error("redis异常：key=[{}]",key,e);
+            }
+
+            @Override
+            public void handleCachePutError(RuntimeException e, Cache cache, Object key, Object value) {
+                logger.error("redis异常：key=[{}]",key,e);
+            }
+
+            @Override
+            public void handleCacheEvictError(RuntimeException e, Cache cache, Object key)    {
+                logger.error("redis异常：key=[{}]",key,e);
+            }
+
+            @Override
+            public void handleCacheClearError(RuntimeException e, Cache cache) {
+                logger.error("redis异常：",e);
+            }
+        };
+        return cacheErrorHandler;
+    }
+}
